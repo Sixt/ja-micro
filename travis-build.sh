c94968c46374b252b2de2b50e9d783d62c16ad5e
Brian Krahmer <brian.krahmer@sixt.com>
2017-02-16 14:30:23 +0100

    Working on release build

--- travis-build.sh
+++ travis-build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 if ([ ! -z "$TRAVIS_TAG" ]); then
-  ./gradlew release -Prelease.useAutomaticVersion=true -Prelease.releaseVersion="$TRAVIS_TAG"
+  ./gradlew release -Prelease.useAutomaticVersion=true -Prelease.releaseVersion="$TRAVIS_TAG" -x preTagCommit -x createReleaseTag
 else
   ./gradlew build
 fi
