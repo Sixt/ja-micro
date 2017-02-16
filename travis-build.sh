838728a5b84089ccac9d5681b5bfd7ec6dc09119
Brian Krahmer <brian.krahmer@sixt.com>
2017-02-16 18:54:00 +0100

    Working on release build

--- travis-build.sh
+++ travis-build.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+git config --global user.email "java-micro@sixt.com"
+git config --global user.name "Travis CI"
+
 TRAVIS_TAG=`git tag --contains`
 
 if ([ -n "${TRAVIS_TAG}" ]); then
