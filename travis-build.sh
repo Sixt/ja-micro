e06c6b9571e5b26173725f2e22d560075fae59ef
Brian Krahmer <brian.krahmer@sixt.com>
2017-02-16 18:42:00 +0100

    Working on release build

--- travis-build.sh
+++ travis-build.sh
@@ -4,7 +4,7 @@ TRAVIS_TAG=`git tag --contains`
 
 if ([ -n "${TRAVIS_TAG}" ]); then
   echo "Running release build"
-  ./gradlew release -Prelease.useAutomaticVersion=true -Prelease.releaseVersion="$TRAVIS_TAG" -x preTagCommit -x createReleaseTag
+  ./gradlew release -Prelease.useAutomaticVersion=true -Prelease.releaseVersion="$TRAVIS_TAG" -x preTagCommit -x createReleaseTag -x checkCommitNeeded
 else
   echo "Running CI build"
   ./gradlew build
