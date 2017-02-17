644d5b929c6a7f8f6709efbfae2a5e3b5e5c0d32
Brian Krahmer <brian.krahmer@sixt.com>
2017-02-17 10:21:31 +0100

    Working on release build

--- travis-build.sh
+++ travis-build.sh
@@ -7,7 +7,11 @@ TRAVIS_TAG=`git tag --contains`
 
 if ([ -n "${TRAVIS_TAG}" ]); then
   echo "Running release build"
-  ./gradlew release -Prelease.useAutomaticVersion=true -Prelease.releaseVersion="$TRAVIS_TAG" -x preTagCommit -x createReleaseTag -x checkCommitNeeded
+  ./gradlew release -Prelease.useAutomaticVersion=true -Prelease.releaseVersion="$TRAVIS_TAG" -x preTagCommit -x createReleaseTag -x checkCommitNeeded -x commitNewVersion
+  git remote set-url origin https://${GITHUB_API_KEY}:github.com/Sixt/ja-micro.git
+  git add gradle.properties
+  git commit -m "Automatic version bump after release"
+  git push origin
 else
   echo "Running CI build"
   ./gradlew build
