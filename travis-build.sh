a90bdc0f95becfcd947b3eaef939a86945baaf49
Brian Krahmer <brian.krahmer@sixt.com>
2017-02-17 11:15:17 +0100

    Give up on automatic version bumping

--- travis-build.sh
+++ travis-build.sh
@@ -1,13 +1,10 @@
 #!/usr/bin/env bash
 
-git config --global user.email "java-micro@sixt.com"
-git config --global user.name "Travis CI"
-
 TRAVIS_TAG=`git tag --contains`
 
 if ([ -n "${TRAVIS_TAG}" ]); then
   echo "Running release build"
-  ./gradlew build bintrayUpload
+  ./gradlew release -Prelease.useAutomaticVersion=true -Prelease.releaseVersion="$TRAVIS_TAG" -x preTagCommit -x createReleaseTag -x checkCommitNeeded -x commitNewVersion
 else
   echo "Running CI build"
   ./gradlew build
