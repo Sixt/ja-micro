f4c69d77d9bfc588b628d32c42b923463cbbb4c8
Thomas Kieffer <thomas.kieffer@codecentric.de>
2017-03-15 16:44:29 +0100

    Add event handler test making use of a service impersonator to SITs

--- sit/src/serviceTest/java/com/sixt/service/test_service/RandomServiceIntegrationTest.java
+++ sit/src/serviceTest/java/com/sixt/service/test_service/RandomServiceIntegrationTest.java
@@ -1,26 +1,30 @@
 /**
  * Copyright 2016-2017 Sixt GmbH & Co. Autovermietung KG
- * Licensed under the Apache License, Version 2.0 (the "License"); you may 
- * not use this file except in compliance with the License. You may obtain a 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may obtain a
  * copy of the License at http://www.apache.org/licenses/LICENSE-2.0
- * Unless required by applicable law or agreed to in writing, software 
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT 
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the 
- * License for the specific language governing permissions and limitations 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
  * under the License.
  */
 
 package com.sixt.service.test_service;
 
+import com.google.gson.JsonObject;
 import com.palantir.docker.compose.DockerComposeRule;
 import com.palantir.docker.compose.configuration.ProjectName;
 import com.sixt.service.framework.health.HealthCheck;
+import com.sixt.service.framework.protobuf.ProtobufUtil;
 import com.sixt.service.framework.rpc.LoadBalancer;
 import com.sixt.service.framework.rpc.RpcCallException;
 import com.sixt.service.framework.rpc.ServiceEndpoint;
 import com.sixt.service.framework.servicetest.helper.DockerComposeHelper;
+import com.sixt.service.framework.servicetest.mockservice.ServiceImpersonator;
 import com.sixt.service.framework.servicetest.service.ServiceUnderTest;
 import com.sixt.service.framework.servicetest.service.ServiceUnderTestImpl;
+import com.sixt.service.test_service.api.TestServiceOuterClass;
 import com.sixt.service.test_service.api.TestServiceOuterClass.GetRandomStringQuery;
 import com.sixt.service.test_service.api.TestServiceOuterClass.RandomStringResponse;
 import com.sixt.service.test_service.api.TestServiceOuterClass.SetHealthCheckStatusCommand;
@@ -32,11 +36,14 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.Timeout;
 
+import java.util.List;
+import java.util.Random;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class RandomServiceIntegrationTest {
 
-    private static ServiceUnderTest randomService;
+    private static ServiceUnderTest testService;
 
     @ClassRule
     public static DockerComposeRule docker = DockerComposeRule.builder()
@@ -49,6 +56,8 @@ public class RandomServiceIntegrationTest {
             .projectName(ProjectName.random())
             .build();
 
+    private static ServiceImpersonator serviceImpersonator;
+
     @Rule
     public Timeout globalTimeout = Timeout.seconds(30);
 
@@ -57,13 +66,15 @@ public class RandomServiceIntegrationTest {
         DockerComposeHelper.setKafkaEnvironment(docker);
         DockerComposeHelper.setConsulEnvironment(docker);
 
-        randomService = new ServiceUnderTestImpl("com.sixt.service.test-service", false);
+        serviceImpersonator = new ServiceImpersonator("com.sixt.service.another-service");
+
+        testService = new ServiceUnderTestImpl("com.sixt.service.test-service", "events");
     }
 
     @Test
     public void testGetRandomString() throws Exception {
         GetRandomStringQuery request = GetRandomStringQuery.newBuilder().build();
-        RandomStringResponse response = (RandomStringResponse)randomService.sendRequest(
+        RandomStringResponse response = (RandomStringResponse) testService.sendRequest(
                 "TestService.GetRandomString", request);
 
         String result = response.getRandom();
@@ -73,7 +84,7 @@ public class RandomServiceIntegrationTest {
     @Test
     public void testSlowRespondingService() {
         try {
-            randomService.sendRequest("TestService.SlowResponder",
+            testService.sendRequest("TestService.SlowResponder",
                     GetRandomStringQuery.getDefaultInstance());
         } catch (RpcCallException ex) {
             ex.printStackTrace();
@@ -87,9 +98,9 @@ public class RandomServiceIntegrationTest {
                 .setStatus(HealthCheck.Status.FAIL.toString()).setMessage(failureMessage).build();
         //we have to work around the normal communication channels, as the load-balancer
         //won't let us talk to a failing instance
-        LoadBalancer loadBalancer = randomService.getLoadBalancer();
+        LoadBalancer loadBalancer = testService.getLoadBalancer();
         ServiceEndpoint endpoint = loadBalancer.getHealthyInstance();
-        randomService.sendRequest("TestService.SetHealthCheckStatus", command);
+        testService.sendRequest("TestService.SetHealthCheckStatus", command);
         String url = "http://" + endpoint.getHostAndPort() + "/health";
         HttpClient httpClient = new HttpClient();
         httpClient.start();
@@ -99,4 +110,19 @@ public class RandomServiceIntegrationTest {
                 "{\"name\":\"test_servlet\",\"status\":\"CRITICAL\",\"reason\":\"" + failureMessage + "\"}]}");
     }
 
+    @Test
+    public void testRandomSampleEventHandler() throws Exception {
+        serviceImpersonator.publishEvent("events", TestServiceOuterClass.RandomSampleEvent.newBuilder()
+                .setId("some-id")
+                .setMessage("The message")
+                .build());
+
+        List<JsonObject> receivedEvents = testService.getAllJsonEvents();
+
+        assertThat(receivedEvents.size()).isEqualTo(1);
+        TestServiceOuterClass.RandomSampleEvent event = ProtobufUtil.jsonToProtobuf(receivedEvents.get(0).toString(),
+                TestServiceOuterClass.RandomSampleEvent.class);
+        assertThat(event.getId()).isEqualTo("some-id");
+        assertThat(event.getMessage()).isEqualTo("The message");
+    }
 }
