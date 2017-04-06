3c8cf693264f1315b8744ef87c1a80ca3cd01fe1
Alexander Buchmann <alexander.buchmann@sixt.com>
2017-04-06 18:16:00 +0200

    Fixing injection and service integration tests

--- src/main/java/com/sixt/service/framework/injection/InjectionModule.java
+++ src/main/java/com/sixt/service/framework/injection/InjectionModule.java
@@ -1,12 +1,12 @@
 /**
  * Copyright 2016-2017 Sixt GmbH & Co. Autovermietung KG
- * Licensed under the Apache License, Version 2.0 (the "License"); you may 
- * not use this file except in compliance with the License. You may obtain a 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may obtain a
  * copy of the License at http://www.apache.org/licenses/LICENSE-2.0
- * Unless required by applicable law or agreed to in writing, software 
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT 
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the 
- * License for the specific language governing permissions and limitations 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
  * under the License.
  */
 
@@ -20,6 +20,7 @@ import com.sixt.service.framework.FeatureFlags;
 import com.sixt.service.framework.MethodHandlerDictionary;
 import com.sixt.service.framework.ServiceProperties;
 import com.sixt.service.framework.configuration.ConfigurationManager;
+import com.sixt.service.framework.kafka.messaging.ConsumerFactory;
 import org.eclipse.jetty.client.HttpClient;
 import org.eclipse.jetty.util.ssl.SslContextFactory;
 import org.slf4j.Logger;
@@ -50,6 +51,7 @@ public class InjectionModule extends AbstractModule {
     protected void configure() {
         bind(ServiceProperties.class).toInstance(serviceProperties);
         bind(HttpClient.class).toInstance(httpClient);
+        bind(ConsumerFactory.class).toProvider(ConsumerFactoryProvider.class);
     }
 
     @Provides
