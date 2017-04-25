8b80a2b529ae9958b6c269c713bec8003536598d
Alexander Buchmann <alexander.buchmann@sixt.com>
2017-04-25 14:19:18 +0200

    Copyright headers. Adjusted comment.

--- src/main/java/com/sixt/service/framework/servicetest/helper/DynamicFileLogCollector.java
+++ src/main/java/com/sixt/service/framework/servicetest/helper/DynamicFileLogCollector.java
@@ -1,3 +1,15 @@
+/**
+ * Copyright 2016-2017 Sixt GmbH & Co. Autovermietung KG
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may obtain a
+ * copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
 package com.sixt.service.framework.servicetest.helper;
 
 
@@ -44,7 +56,6 @@ public class DynamicFileLogCollector implements LogCollector {
         return new DynamicFileLogCollector(new File(path));
     }
 
-
     /**
      * Collect logs of all services listed in the compose files.
      *
@@ -62,7 +73,6 @@ public class DynamicFileLogCollector implements LogCollector {
             collectLogs(service, dockerCompose);
         }
     }
-
 
     /**
      * Incrementally adds newly started services to the set of collected logs.
@@ -94,8 +104,8 @@ public class DynamicFileLogCollector implements LogCollector {
     }
 
     public synchronized void stopCollecting() throws InterruptedException {
-        // TODO: does this shut down policy really work: NO
 
+        // This policy for a graceful shut down does not really work, but since we throw away the JVM, who cares?
         if (!executor.awaitTermination(50L, TimeUnit.MILLISECONDS)) {
             log.debug("docker containers were still running when log collection stopped");
             executor.shutdownNow();
