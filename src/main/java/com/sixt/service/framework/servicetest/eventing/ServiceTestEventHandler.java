a0d7aa72485115ebb5536c704b605d7173234bfd
stepan <stepan.mozyra@sixt.com>
2017-05-13 18:13:21 +0300

    remove unexpected side effect: do not clean expectedEvents

--- src/main/java/com/sixt/service/framework/servicetest/eventing/ServiceTestEventHandler.java
+++ src/main/java/com/sixt/service/framework/servicetest/eventing/ServiceTestEventHandler.java
@@ -1,12 +1,12 @@
 /**
  * Copyright 2016-2017 Sixt GmbH & Co. Autovermietung KG
- * Licensed under the Apache License, Version 2.0 (the "License"); you may 
- * not use this file except in compliance with the License. You may obtain a 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may obtain a
  * copy of the License at http://www.apache.org/licenses/LICENSE-2.0
- * Unless required by applicable law or agreed to in writing, software 
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT 
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the 
- * License for the specific language governing permissions and limitations 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
  * under the License.
  */
 
@@ -39,10 +39,10 @@ public class ServiceTestEventHandler implements EventReceivedCallback<String> {
 
     private static final String GROUP_ID = "service-integration-test";
     private static final int TIMEOUT = 30;
-    private final static int POLL_TIME = 200;
-    private final static int CORE_POOL_SIZE = 1;
-    private final static int MAX_POOL_SIZE = 1;
-    private final static int KEEP_ALIVE_TIME = 15;
+    private static final int POLL_TIME = 200;
+    private static final int CORE_POOL_SIZE = 1;
+    private static final int MAX_POOL_SIZE = 1;
+    private static final int KEEP_ALIVE_TIME = 15;
 
     private final KafkaSubscriberFactory<String> subscriberFactory;
     @VisibleForTesting
@@ -160,9 +160,6 @@ public class ServiceTestEventHandler implements EventReceivedCallback<String> {
         }
 
         executor.shutdownNow();
-
-        // We clear the expected events to be ready for the next testcase
-        expectedEvents.clear();
 
         return foundEvents;
     }
