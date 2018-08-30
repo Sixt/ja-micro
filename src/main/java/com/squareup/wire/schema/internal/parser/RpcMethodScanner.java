b35cb20daba184025892044aef14d83d634b690e
Brian Krahmer <brian.krahmer@sixt.com>
2018-08-30 15:13:52 +0200

    Revert proto class scanning, as it has an issue with speed and reliability

--- src/main/java/com/squareup/wire/schema/internal/parser/RpcMethodScanner.java
+++ src/main/java/com/squareup/wire/schema/internal/parser/RpcMethodScanner.java
@@ -77,15 +77,13 @@ public class RpcMethodScanner {
     }
 
     public List<RpcMethodDefinition> getRpcMethodDefinitions(String serviceName) {
-        // search classpath
-        Map<String, List<RpcMethodDefinition>> jarMap = searchClasspath(serviceName).stream().collect(Collectors.groupingBy(e -> e.getSourceFileName()));
-        // search local path
-        Map<String, List<RpcMethodDefinition>> localMap = searchDirectory(System.getProperty("user.dir"), serviceName).stream().collect(Collectors.groupingBy(e -> e.getSourceFileName()));
+        // first try: search classpath
+        List<RpcMethodDefinition> rpcMethodDefinitions = searchClasspath(serviceName);
 
-        // override jar protos with local protos
-        jarMap.putAll(localMap);
-
-        List<RpcMethodDefinition> rpcMethodDefinitions = jarMap.values().stream().flatMap(List::stream).collect(Collectors.toList());
+        // if rpcMethodDefinitions still empty, : search local directory for proto file
+        if (rpcMethodDefinitions.isEmpty()) {
+            rpcMethodDefinitions = searchDirectory(System.getProperty("user.dir"), serviceName);
+        }
 
         if (rpcMethodDefinitions.isEmpty()) {
             logger.warn("No RPC endpoints found for {}", serviceName);
